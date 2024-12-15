74943732bd6067c480ef11e1f218a01ad36f728c
rjspyk5 <rjspyk5@gmail.com>
2024-12-16 01:40:10 +0600

    basic setup server complete

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const connectDb = require("./config/config");
 const app = express();
 const port = 3000;
+
+app.use(cors());
+
+// middleware
+app.use(express.json());
 
 connectDb();
 app.get("/", (req, res) => {
